445d7fad6ce10e49876bc9a3f33255b6838c4afd
brandon <brandoncottrell54@gmail.com>
2021-10-09 15:48:23 +0000

    question 3

---
+++ Project1 copy/script.js
@@ -0,0 +1,7 @@
+var name = prompt("Please enter your name.", "Name")
+var snack = prompt("What is your favorite snack?","Snack");
+var drink = prompt("What is your favorite drink?","Drink");
+
+document.write("Hi there " + name + ", it seems that you enjoy eating " + snack + " and drinking " + drink + ".");
+
+
